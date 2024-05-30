dffba171ef666a7c65b26a1601fc22140858426a
michaelitindi <michaelitindi@gmail.com>
2024-05-30 19:39:00 +0300

    Delete tests

--- __tests__/stockdata.test.js
+++
@@ -1,6 +0,0 @@
-
-const Stockdata = require('../src/stockdata');
-test('fetchStockData fetches and parses stock data', async () => {
-  const data = await fetchStockData();
-  expect(data).toEqual(expect.any(Object));   
-});
--- __tests__/stocknews.test.js
+++
@@ -1,5 +0,0 @@
-const stocknews = require('../src/stockdata');
-test('fetchStockNews fetches and parses stock data', async () => {
-  const data = await fetchStockNews();
-  expect(data).toEqual(expect.any(Object));   
-});
